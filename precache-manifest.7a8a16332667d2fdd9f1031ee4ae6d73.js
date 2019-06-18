0cc46c651b3a2d3935cbfb41bb5864ced5a5a8d3
Nazar <mobidik460@gmail.com>
2019-06-18 22:26:34 +0300

    Updates

---
+++ precache-manifest.7a8a16332667d2fdd9f1031ee4ae6d73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "e189807efb54b3bea51d",
+    "url": "/todolist/static/js/main.e189807e.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/todolist/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "e189807efb54b3bea51d",
+    "url": "/todolist/static/css/main.d3858880.chunk.css"
+  },
+  {
+    "revision": "85079ee720ba0095888a7ef7f580c612",
+    "url": "/todolist/index.html"
+  }
+];
